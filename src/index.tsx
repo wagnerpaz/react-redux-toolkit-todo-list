9340b1c5e6e366eb7533896c6ceb731154c5d1e9
Wagner Paz <wagner.o.paz@gmail.com>
2021-11-25 08:37:02 -0300

    setup redux & api

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
+import { Provider as ReduxProvider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
